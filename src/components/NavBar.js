4ee175e788e3f3b7923a71ee15c257c0945d2e0e
Elisei-Trifan <slavaberzan@gmail.com>
2024-11-28 21:29:54 +0200

    observer

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext } from 'react'
+
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { Context } from '..'
+import { NavLink } from 'react-router-dom'
+import { SHOP_ROUTE } from '../utils/consts'
+import { Button } from 'react-bootstrap'
+import { observer } from 'mobx-react-lite'
+
+const NavBar = observer(() => {
+  const { user } = useContext(Context)
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>
+          Купи Девайс
+        </NavLink>
+        {user.isAuth ? (
+          <Nav style={{ color: 'white' }} className="ml-auto">
+            <Button variant={'outline-light'}>Админ панель</Button>
+            <Button variant={'outline-light'} className="ms-2">
+              Войти
+            </Button>
+          </Nav>
+        ) : (
+          <Nav style={{ color: 'white' }} className="ms-auto">
+            <Button
+              variant={'outline-light'}
+              onClick={() => user.setIsAuth(true)}
+            >
+              Авторизация
+            </Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
+  )
+})
+
+export default NavBar
