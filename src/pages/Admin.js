3b8b80bc713567f58edbe09f37b4dcc5c0a07f9b
Elisei-Trifan <slavaberzan@gmail.com>
2024-12-03 12:21:01 +0200

    new modals windows

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container } from 'react-bootstrap'
+import CreateType from '../components/modals/CreateType'
+import CreateBrand from '../components/modals/CreateBrand'
+import CreateDevice from '../components/modals/CreateDevice'
 
 const Admin = () => {
+  const [brandVisible, setBrandVisisble] = useState(false)
+  const [typeVisible, setTypeVisisble] = useState(false)
+  const [deviceVisible, setDeviceVisisble] = useState(false)
+
   return (
     <Container className="d-flex flex-column">
-      <Button variant="outline-dark" className="mt-4 p-2">
+      <Button
+        onClick={() => setTypeVisisble(true)}
+        variant="outline-dark"
+        className="mt-4 p-2"
+      >
         Добавить тип
       </Button>
-      <Button variant="outline-dark" className="mt-4 p-2">
+      <Button
+        onClick={() => setBrandVisisble(true)}
+        variant="outline-dark"
+        className="mt-4 p-2"
+      >
         Добавить бренд
       </Button>
-      <Button variant="outline-dark" className="mt-4 p-2">
+      <Button
+        onClick={() => setDeviceVisisble(true)}
+        variant="outline-dark"
+        className="mt-4 p-2"
+      >
         Добавить устройство
       </Button>
+      <CreateType show={typeVisible} onHide={() => setTypeVisisble(false)} />
+      <CreateBrand show={brandVisible} onHide={() => setBrandVisisble(false)} />
+      <CreateDevice
+        show={deviceVisible}
+        onHide={() => setDeviceVisisble(false)}
+      />
     </Container>
   )
 }
